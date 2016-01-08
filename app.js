fb673bae94dd72ee82dabde6b679bb5d2a7919cd
猪野 湧斗 <J00610@joukou.local>
2016-01-08 17:35:43 +0900

    ダメみたいですね…

--- app.js
+++ app.js
@@ -34,27 +34,48 @@ function extractPropur(phrase, callback) {
     }
     var api = 'https://labs.goo.ne.jp/api/entity';
     
-    request.post(api).on('response', function (res) {
-        callback(res);
+    request.post(api, {form: params}, function (err, res, body) {
+        callback(JSON.parse(body));
     });
 }
 
 function fetch(keyword, callback) {
     var params = {
-        q: keyword,
+        q: keyword + ' exclude:retweets',
+        //until: '2015-12-27',
         count: 100,
         result_type: 'recent'
     };
-    client.get('search/tweets', params, function(error, tweets, response){
-        if (typeof callback == 'function') callback(tweets);
-    });
+    
+    var count = 0;
+    var max = 1;
+    var _tweets = [];
+    
+    _fetch();
+    
+    function _fetch() {
+        client.get('search/tweets', params, function(error, tweets, response) {
+            _tweets = _tweets.concat(tweets.statuses);
+            var next_results = tweets.search_metadata.next_results;
+            params = qs.parse(next_results.substring(1));
+            count++;
+            
+            if (count < max) {
+                _fetch();
+            } else {
+                if (typeof callback == 'function') callback(_tweets);
+            }
+        });
+    }
 }
 
 fetch('壊死ニキ', function (tweets) {
-    tweets.statuses.forEach(function (tweet) {
-        setTimeout(function () {
-            //extractKeyphrase(tweet.text, extractPropur);
-            extractPropur(tweet.text, console.log);
-        }, 32);
+    var texts = '';
+    tweets.forEach(function (tweet, index) {
+        texts += tweet.text + '\n\n';
     });
-})
+    extractPropur(texts, function (result) {
+        console.log(texts);
+        console.log(result.ne_list);
+    });
+});
